f2eaa7a9eab0b4c2d0969131c78d4d659c0bf514
revire <riva.revekka@gmail.com>
2020-04-13 12:34:54 +0300

    added header with a link

--- familycards/urls.py
+++ familycards/urls.py
@@ -3,6 +3,7 @@ from .views import IndexView
 
 from . import views
 
+#app_name = 'familycards'
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('member/<slug>', views.view_member, name='view_member'),
